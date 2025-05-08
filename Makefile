5c54f5181a6c418ea56ccfcb3c657b37305076e2
Mahin <mahin.r4586@gmail.com>
2025-05-08 01:57:06 -0500

    file inputoutput and add input file

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	g++ -std=c++11 -Wall -o main.exe main.cpp
+
+clean:
+	rm -f lab10
--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-#include "calculator.hpp"
+int main() {
+    //get file name from user
+    std::string filename;
+    std::cout << "Enter filename: ";
+    std::cin >> filename;
 
-int main(int argc, char *argv[]) {
+    //attempt to open file 
+    std::ifstream infile(filename);
+    if (!infile) {
+        std::cerr << "Error: could not open file.\n";
+        return 1;
+    }
+    //read in file and print out for testing
+    std::string line;
+    int caseNum = 1;
+    while (std::getline(infile, line)) {
+        std::cout << "Case #" << caseNum << ": " << line << "\n";
+        caseNum++;
+    }
 
+    infile.close();
+    return 0;
 }
